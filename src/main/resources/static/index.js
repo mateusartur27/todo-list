7d8c43e493bd00b4482ff419906372b1b7e8fba4
Mateus Artur <mateus2002ns@gmail.com>
2025-05-23 21:09:48 -0300

    teste 4

--- src/main/resources/static/index.js
+++
@@ -1,9 +0,0 @@
-// Redireciona para a página de login se não estiver autenticado
-window.addEventListener('DOMContentLoaded', () => {
-  const authToken = localStorage.getItem('authToken');
-  
-  if (!authToken) {
-    // Se não estiver autenticado, redireciona para a página de login
-    window.location.href = 'login.html';
-  }
-});
